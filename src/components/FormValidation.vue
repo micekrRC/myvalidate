62be916bf5b5b92f192a6ec687e9a9c7932c9938
micekrRC <rmicek@rscsolutions.com>
2019-12-19 16:04:32 -0500

    add firstname field with vuelidate validation att

--- src/components/FormValidation.vue
+++ src/components/FormValidation.vue
@@ -1,4 +1,41 @@
 <template>
+    <div class="card">
+        <h3 class="card-header text-center">Register Area</h3>
+        <div class="card-body">
+            <form>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label>First Name</label>
+                        <!--
+                        $v = validation STATE
+                        grabs properties from data object below
+                        $v.firstname
+                        $v.lastname
+                        ...
+
+                        -->
+                        <!--
+                            https://www.youtube.com/watch?v=SW5OfTAxtDY
+                        4:17 of 14:49	Vuelidate (Form Validation) with Vue.js
+                        -->
+                        <input type="text" class="form-control" 
+                            v-model.trim="$v.firstname.$model" :class="{
+                            'is-invalid' :$v.firstname.$error,
+                            'is-valid':!$v.firstname.$invalid }">                            
+                            <div class="valid-feedback">Your first name is valid !</div>                               
+                            <div class="invalid-feedback">
+                                <span v-if="!$v.firstname.required">First name is required.</span>                                
+                                <span v-if="!$v.firstname.minLength">First name must have at least {{
+                                    $v.firstname.$params.minLength.min }} letters.</span>                                
+                                <span v-if="!$v.firstname.maxLength">First name must have at most {{
+                                    $v.firstname.$params.maxLength.max }} letters.</span>
+                            </div>                               
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
     
 </template>
 
