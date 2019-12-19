01d2c3c1a70a7724bb5bb3dbc27716fb17fce104
micekrRC <rmicek@rscsolutions.com>
2019-12-19 14:36:51 -0500

    create FormValidation vue, with minium basic elements and validation plugin

---
+++ src/components/FormValidation.vue
@@ -0,0 +1,16 @@
+<template>
+    
+</template>
+
+<script>
+
+import { required, minlength, maxlength, between } from 'vuelidate/lib/validators'
+
+export default {
+    
+}
+</script>
+
+<!-- apply styles ONLY to this file -->
+<style scoped>
+</style>
