7b62c1505fb151e9487c82786201df56f09fb32e
micekrRC <rmicek@rscsolutions.com>
2019-12-19 15:37:54 -0500

    new src\components\FormValidation.vue

--- src/components/FormValidation.vue
+++ src/components/FormValidation.vue
@@ -8,6 +8,34 @@ import { required, minlength, maxlength, between } from 'vuelidate/lib/validator
 
 export default {
     
+    name: 'FormValidation',
+    // add state data method
+    data() {  
+            return {    // have it return an object
+                        // different state properties
+                        // '' = null
+                firstname: '',
+                lastname: '',
+                age: 0                
+
+            }
+    },
+    validations: {
+        firstname: {
+            required,
+            minLength: minLength(3),
+            maxLength: maxLength(10)
+        },
+        lastname: {
+            minLength: minLength(5),
+            maxLength: maxLength(12)
+        },
+        age: {
+            between: between(15,40)
+
+        }
+    }
+    
 }
 </script>
 
