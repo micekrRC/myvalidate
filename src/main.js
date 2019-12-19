1aaea34dbd2b7bcf53914e3131349d27a0e818f2
micekrRC <rmicek@rscsolutions.com>
2019-12-19 12:47:35 -0500

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuelidate from 'vuelidate'
+import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
+
 
 
 new Vue({
